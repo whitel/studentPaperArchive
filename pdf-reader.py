fc525df7ad8ad44a71eb82b040f9823e60052f7d
whitel <heriskent@gmail.com>
2022-11-04 21:48:56 +0800

    添加异常处理（未完善）

--- pdf-reader.py
+++ pdf-reader.py
@@ -15,21 +15,46 @@ def fetchAllFile(path):
     return files
 
 """
-提取学号并重命名文件
+清理数据，组装成一个dict
 """
-def extractNumber(filePath):
+def cleanData(text):
+    text = text.replace(' ', '')
+    text = text.replace('\n\n\n\n\n', '\n')
+    text = text.replace('\n\n\n\n', '\n')
+    text = text.replace('\n\n\n', '\n')
+    text = text.replace('\n\n', '\n')
+    text = text.replace('姓\n名：\n', '姓名：')
+    text = text.replace('学\n号：\n', '学号：')
+    text = text.replace('导\n师：\n', '导师：')
+    text = text.replace('学\n院：\n', '学院：')
+    text = text.replace('题\n目：\n', '题目：')
+    text = text.replace('\n年', '年')
+    text = text.replace('\n月', '月')
+    text = text.replace('\n日', '日')
+
+    lines = []
+    for line in text.splitlines():
+        if(line == '' or line.find('保密期限') != -1):
+            continue
+        lines.append(line)
+
+    result = {}
+    for line in lines:
+        if(line.find('：') != -1):
+            temp = line.split('：')
+            result[temp[0]] = temp[1]
+        elif(line.find('年') != -1 and line.find('月') != -1 and line.find('日') != -1):
+            result["日期"] = line
+
+    return result
+
+def getNumber(filePath):
     file = open(filePath, 'rb')
     fileReader = PyPDF2.PdfFileReader(file)
     page = fileReader.pages[0]
     text = page.extract_text(0)
-    numberIndex = text.find("号：\n") + 3
-    
-    if(numberIndex == 2):
-        print("error finding number")
-        return -1
-    else:
-        print(text[numberIndex : numberIndex + 10])
-        return text[numberIndex : numberIndex + 10]
+    d = cleanData(text)
+    return d['学号']
 
 def main(src, dest):
     docPath = src
@@ -41,17 +66,17 @@ def main(src, dest):
         os.makedirs(pdfPath)
     files = fetchAllFile(docPath)
     for file in files:
-        identifier = file.split('\\')[-2]
+        identifier = file.split('/')[-2]
         fileName = os.path.splitext(os.path.basename(file))[0] + '.pdf'
         if not os.path.exists(os.path.join(pdfPath, identifier)):
             os.mkdir(os.path.join(pdfPath, identifier))
         savePath = os.path.join(pdfPath, identifier, fileName)
         if not os.path.exists(savePath):
             print("文件：", file)
-            number = extractNumber(file)
-            os.rename(file, number + '.pdf')
+            number = getNumber(file)
+            os.rename(file, src + '/' + str(number) + '.pdf')
         else:
             print("文件已经存在，无需转换")
         
 if __name__=='__main__':
-    main('./data', './data/result')
+    main('./test', './test/result/')
